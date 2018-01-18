6200a8e8369d09293ccb7aad7f22a93de2636603
Michal <hrdymichal@gmail.com>
2018-01-18 14:47:25 +0100

    2 Commit

--- app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
+++ app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
@@ -13,14 +13,12 @@ public class CurrentRatesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_rates);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add currency filter here", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- app/src/main/java/com/hrdym/exchangerates/MainActivity.java
+++ app/src/main/java/com/hrdym/exchangerates/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hrdym.exchangerates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCurrentRatesButton();
+        //setHistoricalRatesButton();
     }
 
+    private void setCurrentRatesButton() {
+        Button currentER = (Button) findViewById(R.id.buttonCurrent);
+        currentER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CurrentRatesActivity.class));
+            }
+        });
+    }
+/*
+    private void setHistoricalRatesButton() {
+        Button currentER = (Button) findViewById(R.id.buttonHistoric);
+
+    }
+*/
 
 }
