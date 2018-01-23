472abb84760961933cda6e7bf652925c7f52fb33
Michal <hrdymichal@gmail.com>
2018-01-23 15:41:08 +0100

    Create new Dialog before filling the spinner

--- app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
+++ app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
@@ -1,9 +1,12 @@
 package com.hrdym.exchangerates;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Xml;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -13,26 +16,21 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CurrentRatesActivity extends AppCompatActivity {
 
-    //
-    String urlBaseString = new String("https://api.fixer.io/latest?base=");
-    ArrayList<String> currencies;
+    //String urlBaseString = new String("https://api.fixer.io/latest?base=");
+    ArrayList<String> currencyList;
+    private final static String url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 
-    //
-
-    public InputStream getInputStream(URL url) {
-        try {
-            return url.openConnection().getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public CurrentRatesActivity() throws MalformedURLException {
     }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,47 +46,118 @@ public class CurrentRatesActivity extends AppCompatActivity {
             }
         });
 
+        new GetRatesDataTask(this, url).execute();
+    }
 
-/*
-        // parse url to get currencies  for spinner
-        try {
-            URL url = new URL("https://api.fixer.io/latest");
+    public void callBackData(String[] result) {
 
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(false);
-
-            XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(getInputStream(url), "UTF_8");
-
-            boolean insideRates = false;
-
-            // Returns the type of current event
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-
-                if (xpp.getName().equalsIgnoreCase("base")) currencies.add(xpp.getText());
-
-                //if (xpp.getName().equalsIgnoreCase("rates")) currencies.add(xpp.);
-
-                eventType = xpp.next(); //move to next element
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
-/*
+        String[] test = result;
+            /*
         Spinner spinner = (Spinner) findViewById(R.id.spinnerFrom);
         currencies.add("jedna");
         currencies.add("dve");
         ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, currencies);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-*/
+            */
+    }
+}
+
+class GetRatesDataTask extends AsyncTask<String[], Void, String[]> {
+
+    private CurrentRatesActivity activity;
+    private String url;
+    private XmlPullParserFactory xmlFactoryObject;
+    private ProgressDialog pDialog;
+
+    public GetRatesDataTask(CurrentRatesActivity activity, String url) {
+        this.activity = activity;
+        this.url = url;
     }
 
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pDialog = new ProgressDialog(activity);
+        pDialog.setTitle("Getting Information from WEB");
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+    }
+
+    @Override
+    protected String[] doInBackground(String[]... params) {
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+
+            xmlFactoryObject = XmlPullParserFactory.newInstance();
+            XmlPullParser myParser = xmlFactoryObject.newPullParser();
+
+            myParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            myParser.setInput(stream, null);
+            String[] result = parseXML(myParser);
+            stream.close();
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String[] parseXML(XmlPullParser myParser) {
+
+        int event;
+        int count=0;
+        String text = null;
+        ArrayList<String> currencyList = new ArrayList<String>();
+        ArrayList<String> rateList = new ArrayList<String>();
+
+        try {
+            event = myParser.getEventType();
+            while (event != XmlPullParser.END_DOCUMENT) {
+                String name = myParser.getName();
+
+                switch (event) {
+                    case XmlPullParser.START_TAG:
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        //text = myParser.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (name.equals("Cube")) {
+                            if (myParser.getAttributeCount() == -1) break;
+                            currencyList.add(myParser.getAttributeValue(0));
+                            rateList.add(myParser.getAttributeValue(1));
+                        }
+                        break;
+                }
+                event = myParser.next();
+            }
+            //join and return string array
+            currencyList.addAll(rateList);
+            String[] result = new String[currencyList.size()];
+            result = currencyList.toArray(result);
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String[] result) {
+        //call back data to main thread
+        pDialog.dismiss();
+        activity.callBackData(result);
+    }
 }
