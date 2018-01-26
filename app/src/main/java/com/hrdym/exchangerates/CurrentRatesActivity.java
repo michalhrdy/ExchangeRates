3a5ead12f3a37cf6667c688124ace19b4082f57a
Michal <hrdymichal@gmail.com>
2018-01-26 15:02:49 +0100

    Added currency filter dialog

--- app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
+++ app/src/main/java/com/hrdym/exchangerates/CurrentRatesActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
